c1ab46664734dbdcf849bbf10d13084437710b6b
harshitha7151 <harshithar2006@gmail.com>
2025-11-29 15:24:14 +0530

    add

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return render_template("register.html")
+    return render_template('login.html')
 
-@app.route("/register")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
 def register():
-    return render_template("register.html")
+    return render_template('register.html')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
